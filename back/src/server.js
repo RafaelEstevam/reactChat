b964a727110db1af55745180c4567d17b9eda06b
rafaelEstevam <rafael.estevamdesign@gmail.com>
2021-11-01 22:27:19 -0300

    ajust

--- back/src/server.js
+++
@@ -1,7 +0,0 @@
-import {http} from './http';
-import './websocket/client';
-import './websocket/attendant';
-
-http.listen(3000, () => {
-    console.log("Servidor de pé.");
-});
--- typeBack/src/database/index.ts
+++
@@ -1,4 +0,0 @@
-//Cria a conexão com o tupeorm configurada no arquivo ormconfig.json
-import { createConnection } from 'typeorm';
-
-createConnection();
